9984ab1c0d89edf815971cd7c5227b2baec51699
cristianladoglz <cris.1998@hotmail.es>
2019-06-16 08:40:04 +0200

    Prueba integración continua

---
+++ test/test.js
@@ -0,0 +1,9 @@
+require('chai');
+var assert = require('assert');
+const auth = require('solid-auth-client');
+
+describe('Simple test', function () {
+  it('2 + 2 = 4', function () {
+    assert.equal(2+2, 4);
+  })
+})
